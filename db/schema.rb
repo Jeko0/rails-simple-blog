14d79fd13054edc0b1f0b1d85b060730a827ada9
Jeko0 <japulava@unisens.ge>
2022-05-31 15:59:06 +0400

    migrated new database

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_05_31_064302) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.string "content"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
