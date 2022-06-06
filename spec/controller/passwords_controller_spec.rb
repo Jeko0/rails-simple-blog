7a932e637d398127d568092fed7d7f03192a942e
Jeko0 <japulava@unisens.ge>
2022-06-06 16:50:17 +0400

    modified passwords spec

--- spec/controller/passwords_controller_spec.rb
+++ spec/controller/passwords_controller_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe PasswordsController, type: :controller do
     it { should use_before_action(:logged_in?) } 
   end
   
-  context "#edit" do
+  context "GET #edit" do
     before { get :edit }
     it { should respond_with(:found)}
   end
   
-  context "#update" do
+  context "PATCH #update" do
     it { expect(response).to have_http_status(:success) }
   end
 end
