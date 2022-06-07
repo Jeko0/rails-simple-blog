123e8bab5bb013286b3bbbd5cba9ff6487a37d9e
Jeko0 <japulava@unisens.ge>
2022-06-07 15:50:30 +0400

    tested password update

--- spec/controller/passwords_controller_spec.rb
+++ spec/controller/passwords_controller_spec.rb
@@ -1,16 +1,25 @@
 require "rails_helper"
 
 RSpec.describe PasswordsController, type: :controller do
-  context "before action" do
-    it { should use_before_action(:logged_in?) } 
-  end
-  
+  let(:user) { User.create(email: "some@example.com", password: "password", password_confirmation: "password")}
+  before {session[:user_id] = user.id}
+
   context "GET #edit" do
-    before { get :edit }
-    it { should respond_with(:found)}
+    before {get :edit}
+    it { should render_template(:edit) } 
   end
-  
+
   context "PATCH #update" do
-    it { expect(response).to have_http_status(:success) }
+    it "should update database" do 
+      patch :update, params: {id: user.id, user: {password: "newpassword", password_confirmation: "newpassword"} }
+      expect(user.reload.password).to eq(user.password) 
+    end
+  end
+
+  context "PATCH  invalid #update" do
+    it "should not update database" do 
+      patch :update, params: {id: user.id, user: {password: nil, password_confirmation: nil} }
+      expect(user.reload.password).to eq("password") 
+    end
   end
 end
